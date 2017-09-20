d6ae3c1026ffec3e4dc838d6486c0763d9543bb5
kushankavinda <kaavinda92@gmail.lk>
2017-09-20 20:55:04 +0530

    0.2 add time thread

--- public_html/js/playGame.js
+++ public_html/js/playGame.js
@@ -1,35 +1,41 @@
 var groundDiv = document.getElementById("playGround");
-var rSize=[11,22,33];
-var canvas=d3.select(".playGround")
+var score=0;
+var rSize = [11, 22, 33];
+var canvas = d3.select(".playGround")
         .append("svg")
-        .attr("width",'100%')
-        .attr("height",'100%'); 
+        .attr("width", '100%')
+        .attr("height", '100%');
 
-var x1=50;
-var y1=50;
+var x1 = 50;
+var y1 = 50;
 
 var circle = canvas.selectAll("circle")
         .data("rSize")
         .enter()
         .append("circle")
-        .attr("r",30)
-        .attr("cx",x1+'%')
-        .attr("cy",y1+'%')
+        .attr("r", 30)
+        .attr("cx", x1 + '%')
+        .attr("cy", y1 + '%')
         .attr("fill", "red")
-        .on('click',nextCircle);
+        .on('click', nextCircle);
+var a, n;
+function nextCircle() {
+    a = new Date();
+    n = a.getTime();
 
-function nextCircle(){ 
-        var a=new Date();
-        var n=a.getTime();
-        circle.attr("cx",Math.floor((Math.random()*100) +1)+'%')
-        circle.attr("cy",Math.floor((Math.random()*100) +1)+'%')
-        circle.on('click',nextCircle);
-        var b=new Date();
-        var c=b.getTime();
-        var elapsed = c-n;
-        if(elapsed>1000){
-           // alert("1000 >");
-       }else{
-         //  alert("1000<")
-       }
+    circle.attr("cx", Math.floor((Math.random() * 100) + 1) + '%')
+    circle.attr("cy", Math.floor((Math.random() * 100) + 1) + '%')
+    circle.on('click', timeThread);
+    var elapsed = c - n;
 }
+var c, b;
+function timeThread() {
+    b = new Date();
+    c = b.getTime();
+    if((c-n)<2000){
+        nextCircle();
+    }else{
+        alert("Time passed");
+    }
+}
+
